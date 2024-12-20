00b1aa9541d9eca59398343c33c8844a0a5fec1a
Tupac Zuleta <tupakzl@gmail.com>
2024-12-20 12:28:05 -0300

    voting updated

--- app/voting_condorcet.py
+++ app/voting_condorcet.py
@@ -57,20 +57,31 @@ def condorcet_winner(ballots):
             respuesta += f"El ganador de Condorcet es: {candidates[i]}\n"
             return respuesta
 
-    return None
+    respuesta += f"El ganador de Condorcet es: None\n"
+    return respuesta
 
 
 # Ejemplo de uso:
-ballots = [
-    ["A", "B", "C"],
-    ["B", "C", "A"],
-    ["B", "C", "A"],
-    ["B", "C", "A"],
-    ["B", "C", "A"]
-]
+
+with open('app/data/data.txt', 'r') as file:
+        data = []
+        for line in file:
+            if line.strip():
+                name, ordered_list, fechahora = line.strip().split('; ')
+                #Obtengo dentro de ordered list los valores que están entre ''
+                ordered_list = ordered_list.split(': ')[1]
+                ordered_list = ordered_list[1:-1].split("', '")
+                # Dentro de cada item en ordered_list, elimino las comillas simples y dobles
+                for i in range(len(ordered_list)):
+                    ordered_list[i] = ordered_list[i].replace("'", "")
+                    ordered_list[i] = ordered_list[i].replace('"', "")
+                data.append(ordered_list)
+
+ballots = data
+
 
 winner = condorcet_winner(ballots)
 if winner:
-    print(f"\nEl ganador de Condorcet es: {winner}")
+    print(f"\n{winner}")
 else:
     print("\nNo existe un ganador de Condorcet")
--- app/voting_irv.py
+++ app/voting_irv.py
@@ -39,13 +39,21 @@ def irv_winner(ballots):
 
 
 # Ejemplo de uso:
-ballots = [
-    ["A", "C", "B"],
-    ["B", "C", "A"],
-    ["C", "A", "B"],
-    ["C", "B", "A"],
-    ["A", "B", "C"]
-]
+with open('app/data/data.txt', 'r') as file:
+        data = []
+        for line in file:
+            if line.strip():
+                name, ordered_list, fechahora = line.strip().split('; ')
+                #Obtengo dentro de ordered list los valores que están entre ''
+                ordered_list = ordered_list.split(': ')[1]
+                ordered_list = ordered_list[1:-1].split("', '")
+                # Dentro de cada item en ordered_list, elimino las comillas simples y dobles
+                for i in range(len(ordered_list)):
+                    ordered_list[i] = ordered_list[i].replace("'", "")
+                    ordered_list[i] = ordered_list[i].replace('"', "")
+                data.append(ordered_list)
+
+ballots = data
 
 winner = irv_winner(ballots)
 print(f"El ganador por IRV es: {winner}")
