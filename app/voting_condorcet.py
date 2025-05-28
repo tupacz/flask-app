c9cfd56c53f6080a3cc4b0cb39dd4a2ce360a83c
Tupac Zuleta <tupakzl@gmail.com>
2025-05-28 12:59:14 -0300

    Refactor ordered list parsing to use ast.literal_eval for improved safety and clarity

--- app/voting_condorcet.py
+++ app/voting_condorcet.py
@@ -133,7 +133,7 @@ def get_condorcet_winner(ruta_archivo: str):
                 name, ordered_list, fechahora = line.strip().split('; ')
                 #Obtengo dentro de ordered list los valores que están entre ''
                 ordered_list = ordered_list.split(': ')[1]
-                ordered_list = ordered_list[1:-1].split("', '")
+                ordered_list = ast.literal_eval(ordered_list)
                 # Dentro de cada item en ordered_list, elimino las comillas simples y dobles
                 for i in range(len(ordered_list)):
                     ordered_list[i] = ordered_list[i].replace("'", "")
